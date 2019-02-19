23162677b3d0f246f2153e5141b66de553609359
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-02-19 17:42:10 -0300

    Add js function to render polygon preview
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- app/controllers/geographies_controller.rb
+++ app/controllers/geographies_controller.rb
@@ -3,7 +3,15 @@ class GeographiesController < ApplicationController
   before_action :set_preview_geography, only: :preview_polygon
 
   def preview_polygon
+    geojson_data = params['geojson']
 
+    @preview_geography.geojson = geojson_data
+
+    if @preview_geography.valid?
+      render "preview_map"
+    else
+      render "redlight_input"
+    end
   end
 
   private
--- config/routes/geography.rb
+++ config/routes/geography.rb
@@ -1,1 +1,1 @@
-get '/preview_polygon', to: 'geography#preview_polygon'
+post '/preview_polygon', to: 'geographies#preview_polygon'
